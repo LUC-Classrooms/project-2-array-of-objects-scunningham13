a845185e4b9a58288f2b2668b7a0c86678a89635
scunningham13 <156953717+scunningham13@users.noreply.github.com>
2024-04-24 19:14:09 +0000

    project two complete

--- script.js
+++ script.js
@@ -11,16 +11,68 @@
 ***/
 
 // Global Variables go here
-
+var fly, fly2
+var dragonflies =new Array(10);
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600 x 400 pixel drawing canvas
-
-
+  fly = new Dragonfly (width/2, height/2);
+  fly2 = new Dragonfly(width/4, height/2);
+  for(let i=0;i<dragonflies.length; i++){ 
+    dragonflies[i]=new Dragonfly(random(width),random(height))
+  }
 }
 
 function draw(){
   background(200); //light gray background
-  
+  //fly.display()
+  fly.move()
+  //fly2.display()
+  fly2.move()
+  for(let i=0;i<dragonflies.length; i++){ 
+    dragonflies[i].display()
+    dragonflies[i].move()
+  }
 }
 
+function Dragonfly(_x,_y) { 
+  this.x = _x
+  this.y = _y
+  this.a = 0;
+  this.xSpeed = random(-2,2);
+  this.ySpeed = random(-2,2);
+  
+  this.move = function(){
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    if(this.x<0 || this.x>width){
+      this.xSpeed *= -1;
+    }
+    if(this.y<0 || this.y>height){
+      this.ySpeed *= -1;
+    }
+  }
+
+  this.display = function (){
+    push ();
+    translate (this.x, this.y);
+    rotate(this.a);
+    // add your image drawing code here
+    fill (250, 0, 250);
+    ellipse (0, 0, 35, 165);
+    fill (120, 150, 250);
+    ellipse (-90, -25, 150, 30); // wings
+    ellipse (90, -25, 150, 30);
+    ellipse (-90, 5, 150, 30);
+    ellipse (90, 5, 150, 30);
+    line (0, -83, -15, -125); // antennas 
+    line (0, -82, 15, -125);
+    ellipse(-15, -125, 8, 8);
+    ellipse(15, -125, 8, 8);
+    fill (0); // eyes
+    ellipse (-5, -50, 3, 3);
+    ellipse (5, -50, 3, 3);
+    pop ();
+  }
+}
